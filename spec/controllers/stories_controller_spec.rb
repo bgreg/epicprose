32dc2d38016715412c88ae173c919a55ae2275ad
greg@vorstack.com <bgregmc@icloud.com>
2014-03-23 13:08:13 -0700

    added a valid attribute to the standard controller test

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -23,7 +23,8 @@ describe StoriesController do
   # This should return the minimal set of attributes required to create a valid
   # Story. As you add validations to Story, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString", "picture_url" => "http://www.google.com"} }
+
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
