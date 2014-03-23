d6015eb57271b6bab44c99c73b4160109da00067
greg@vorstack.com <bgregmc@icloud.com>
2014-03-23 13:07:33 -0700

    class names were wrong

--- spec/helpers/chapter_helper_spec.rb
+++ spec/helpers/chapter_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe ChapterHelper do
+describe ChaptersHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/helpers/story_helper_spec.rb
+++ spec/helpers/story_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe StoryHelper do
+describe StoriesHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
