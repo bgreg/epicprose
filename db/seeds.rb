b1a2423a07cdead86144b155068353b491478cbc
greg@vorstack.com <bgregmc@icloud.com>
2014-03-23 19:59:02 -0700

    added genre list to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 u = User.new(
-  :email => "seed.mcseed_1@gmail.com",
-  :password => '12341234'
+  email: "seed.mcseed_1@gmail.com",
+  password: '12341234'
 )
 u.save!(:validate => false)
+
+File.open('db/genres').each do |line|
+  Genre.create(body: line.squish)
+end
