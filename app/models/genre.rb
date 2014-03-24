d5e2ce19ca5c3503a87fdff15abd969d2f280d46
greg@vorstack.com <bgregmc@icloud.com>
2014-03-23 21:36:08 -0700

    moved random genre action into genre model, made more sense here

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,9 @@
 class Genre < ActiveRecord::Base
   validates_presence_of :body
+
+  def self.random_genre
+    Genre.limit(1).offset(rand(Genre.count)).first
+  end
 private
   def genre_params
     params.require(:body).permit(:body)
