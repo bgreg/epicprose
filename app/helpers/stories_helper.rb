e2423996814045bc1333fe40b2c9457a43311a5d
greg@vorstack.com <bgregmc@icloud.com>
2014-04-05 14:05:53 -0700

    style changes and simplified getting user turns, I wasn't setting turn in story create and just noticed that. So this method doesn't need nil checks anymore

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,17 +1,16 @@
 module StoriesHelper
-  def get_co_author(story_id)
-    StoryRole.where(story_id: story_id, role: :co_author).first.user.email
+  def get_co_author( story_id )
+    StoryRole.where( story_id: story_id, role: :co_author ).first.user.email
   end
 
-  def get_author(story_id)
+  def get_author( story_id )
     StoryRole.where(story_id: story_id, role: :author).first.user.email
   end
 
-  def get_user(id)
-    id && User.find(id).email
+  def get_user_turn( id )
+    turn = "Your turn"
+    turn =  User.find( id ).email if id != current_user.id
+    turn
   end
 
-  def get_turn(id)
-    id || current_user.id
-  end
 end
